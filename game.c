1295317729ca5cc0097d7f63db9ec3a57aefa353
Austin Bagwell <austinbagwell.dev@gmail.com>
2023-10-13 19:48:37 -0400

    print basic title stuff

---
+++ game.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main () {
+  char title[] = "==== << C Shanty  >> ====\n";
+  char subTitle[] = "a text based adventure\n";
+  printf("%s\n", title);
+  printf("%s\n", subTitle);
+
+  return 0;
+}
