970a41cde44f6abdf8f93799ad4b75480af6ffb6
Taylor Barringer <144394091+TaylorBarringer@users.noreply.github.com>
2023-10-15 13:32:26 -0500

    add print statement (#6)

--- game.c
+++ game.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
 
-int main () {
+int main()
+{
   char title[] = "==== << C Shanty  >> ====\n";
   char subTitle[] = "a text based adventure\n";
   printf("%s\n", title);
   printf("%s\n", subTitle);
+
+  printf("I love you");
 
   return 0;
 }
