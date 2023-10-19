ab0426bf84bb528a17bb6cecd3562f9bb7a62f06
Austin Bagwell <austinbagwell.dev@gmail.com>
2023-10-18 21:08:39 -0400

    add 2 super basic not-functional functions

--- game.c
+++ game.c
@@ -11,3 +11,20 @@ int main()
 
   return 0;
 }
+
+// TODO - figure out what syntax to use for function parameter
+// takes one argument: an array of strings
+// prints them to the console
+void printInputOptions(char *options) {
+  printf("%s\n", options);
+}
+
+// TODO - rework entirely, mostly just here so I have a baseline
+int getUserInput() {
+  int myNum;
+  printf("==> ");
+  scanf("%d", &myNum);
+  // TODO
+  // typecheck myNum, handle any errs
+  return myNum;
+}
